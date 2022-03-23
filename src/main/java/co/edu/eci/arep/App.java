0b9b580ed5494c90a40433af310b5cd507fbc20d
Kevin Garzón <kevinegp13@gmail.com>
2022-03-23 14:14:48 -0500

    Se toma el keystorepath y keystorepassword desde variables de entorno

--- src/main/java/co/edu/eci/arep/App.java
+++ src/main/java/co/edu/eci/arep/App.java
@@ -4,8 +4,30 @@ import static spark.Spark.*;
 
 public class App {
     public static void main( String[] args ) {
-        port(5000);
-        secure("keystores/ecikeystore.p12", "123456", null, null);
+        port(getPort());
+        secure(getKeyStorePath(), getKeyStorePassword(), null, null);
         get("/hello", (req, res) -> "Hello Heroku");
     }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+    static String getKeyStorePath() {
+        if (System.getenv("KEYSTOREPATH") != null) {
+            return System.getenv("KEYSTOREPATH");
+        }
+        return "keystores/ecikeystore.p12";
+    }
+
+    static String getKeyStorePassword() {
+        if (System.getenv("KEYSTOREPASSWORD") != null) {
+            return System.getenv("KEYSTOREPASSWORD");
+        }
+        return "123456";
+    }
+    
 }
